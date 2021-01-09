133b538d4a0195a81d63720708b939203cc83208
Christopher <christopher@Christophers-MacBook-Pro.local>
2021-01-09 18:52:53 -0500

    USGS Data

--- static/js/logicStep1.js
+++ static/js/logicStep1.js
@@ -33,11 +33,10 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/<ChrisBarton107>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/<ChrisBarton107>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
